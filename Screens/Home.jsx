8aeb94b4369735a953392d0866718ddd7cf54b91
Darshan S <hitmandarshan34@gmail.com>
2024-06-10 00:25:10 +0530

    2nd cammit

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -2,12 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import Header from "../components/home/Header";
 import SearchBar from "../components/home/SearchBar";
+import Slider from "../components/home/Slider";
 
 const Home = () => {
   return (
     <View className="flex-1 mt-5 p-4">
       <Header />
       <SearchBar setSearchText={(value) => console.log(value)} />
+      <Slider />
     </View>
   );
 };
